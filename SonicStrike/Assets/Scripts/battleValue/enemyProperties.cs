ae6f72b93ffffe81aa8f0bb48c5a169e84b58ad8
Chaojiyuanshendawang <1085893746@qq.com>
2025-05-19 12:39:34 +0800

    wywaawwwlwlbb

--- SonicStrike/Assets/Scripts/battleValue/enemyProperties.cs
+++ SonicStrike/Assets/Scripts/battleValue/enemyProperties.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class enemyProperties : properties
 {
+    [SerializeField] private Image yuanShen;
     public override void hurt(float damage)
     {
         currentHealth -= damage;
         if (currentHealth <= 0)
         {
+            yuanShen.color = Color.yellow;
             healthyBar.updateValue(0);
             levelGlobalControl.instance.moneyAcquire += Mathf.RoundToInt(damage);
             healthyBar.updateValue(levelGlobalControl.instance.moneyAcquire, "Point:");
